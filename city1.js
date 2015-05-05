0def9669e91e20282d280089add09f6835cd764c
denistanwh <denistanwh@gmail.com>
2015-05-05 13:15:18 -0400

    test

--- city1.js
+++ city1.js
@@ -102,7 +102,7 @@ var lowerOuterArea = d3.svg.area()
 		.attr('clip-path', 'url(#rect-clip)')
 		.attr("fill", "rgba(100, 100, 100, 0.1)")
 	.attr("stroke", "rgba(100, 100, 100, 0.1)")
-	.call(d3.helper.tooltip("95 % Confidence Interval"));
+	.call(d3.helper.tooltip("<b>95 % Confidence Interval</b>"));
 	       //function(d, i){
 	         //return "<b>95th %: "+d.Hi95 + "<br/>80th %: "+d.Hi80 + "</b>";
 	       //}
