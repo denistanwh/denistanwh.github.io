ec19792db7589676ed3cc6ec8d3013598f3f5153
denistanwh <denistanwh@gmail.com>
2015-05-07 23:40:32 -0400

    Test tooltip

--- city1.js
+++ city1.js
@@ -109,19 +109,7 @@ var lowerOuterArea = d3.svg.area()
 	       }
 	   ));
 	   
-svg.selectAll("dot")	
-        .data(data)			
-    .enter().append("circle")								
-        .attr("r", 5)		
-        .attr("cx", function(d) { return x(d.date); })		 
-        .attr("cy", function(d) { return yTwo(d.act); })
-	   	//.style("fill", "none")
-	.call(d3.helper.tooltip(
-	       function(d, i){
-	         //return "<b>Energy Demand</b>"
-			 return "<b>Energy Demand: <br>"+d.act + " MW </b>";
-	       }
-	   ));
+
 
 	svg.append('path')
 		.attr('class', 'area lower outer')
@@ -182,6 +170,20 @@ svg.selectAll("dot")
 				 //return "<b>95th %: "+d.Hi95 + "<br/>80th %: "+d.Hi80 + "</b>";
 		      // }
 		   //));
+
+	   svg.selectAll("dot")	
+	           .data(data)			
+	       .enter().append("circle")								
+	           .attr("r", 5)		
+	           .attr("cx", function(d) { return x(d.date); })		 
+	           .attr("cy", function(d) { return yTwo(d.act); })
+	   	   	.style("fill", "none")
+	   	.call(d3.helper.tooltip(
+	   	       function(d, i){
+	   	         //return "<b>Energy Demand</b>"
+	   			 return "<b>Energy Demand: <br>"+d.act + " MW </b>";
+	   	       }
+	   	   ));
 		
 	svg.append('path')
 		.attr('class', 'figure')
