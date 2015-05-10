65a49de3c7a3a124b38e99426b1ec84384f1cbd2
denistanwh <denistanwh@gmail.com>
2015-05-09 22:49:17 -0400

    test

--- cluster1.js
+++ cluster1.js
@@ -117,7 +117,7 @@ function startTransitions (svg, chartWidth, chartHeight, rectClip, x) {
 }
 
 function makeChart (data) {
-	var svgWidth  = 960,
+	var svgWidth  = 800,
 		svgHeight = 300,
 		margin = { top: 20, right: 20, bottom: 40, left: 60 },
 		chartWidth  = svgWidth  - margin.left - margin.right,
