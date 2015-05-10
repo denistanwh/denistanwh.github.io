d56454ee3c3f78304dc24ce76f997e99911c4de7
denistanwh <denistanwh@gmail.com>
2015-05-09 22:15:15 -0400

    Update

--- cluster1.js
+++ cluster1.js
@@ -130,7 +130,7 @@ function makeChart (data) {
 		yAxis = d3.svg.axis().scale(y).orient('left')
 				.innerTickSize(-chartWidth).outerTickSize(0).tickPadding(10);
 
-	var svg = d3.select("#testViz").append('svg')
+	var svg = d3.select("#ClusterViz").append('svg')
 		.attr('width',  svgWidth)
 		.attr('height', svgHeight)
 		.append('g')
